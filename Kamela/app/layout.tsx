3da4cef02119422f38922ff4066bcca77dcd1d6b
Neo Chuene <neomautjane@gmail.com>
2026-02-12 14:08:54 +0200

    Added vercel monitoring and analytics tools

--- Kamela/app/layout.tsx
+++ Kamela/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/footer/footer";
 import "remixicon/fonts/remixicon.css";
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -77,6 +79,8 @@ export default function RootLayout({
 
         <Navbar/>
         {children}
+        <Analytics />
+        <SpeedInsights />
         <Footer/>
       </body>
     </html>
